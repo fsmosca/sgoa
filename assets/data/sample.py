7f35a6263e13c078fdc036d8c8f09514ea26d274
fsmosca <fsmosca@users.noreply.github.com>
2024-01-24 21:27:24 +0800

    Create sample.py

---
+++ assets/data/sample.py
@@ -0,0 +1,5 @@
+data = {
+    'Name': ['John Doe', 'Jane Smith', 'Alex Johnson', 'Maria Garcia', 'David Brown'],
+    'Email': ['johndoe@example.com', 'janesmith@mail.com', 'alexjohnson@domain.com', 'mariagarcia@service.com', 'davidbrown@website.com'],
+    'Phone Number': ['123-456-7890', '234-567-8901', '345-678-9012', '456-789-0123', '567-890-1234']
+}
