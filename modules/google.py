0eb75263837026c8f29925565f399e19f0434aa2
fsmosca <fsmosca@users.noreply.github.com>
2024-01-28 05:12:46 +0800

    Update google.py

--- modules/google.py
+++ modules/google.py
@@ -14,7 +14,7 @@ import requests
 import streamlit as st
 
 
-REDIRECT_URI = 'http://localhost:8501/auth'
+REDIRECT_URI = 'https://risoftcontact.streamlit.app/auth'
 GOOGLE_CLIENT_ID = st.secrets["CLIENT_ID"]
 GOOGLE_CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
